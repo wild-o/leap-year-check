8d565419e46a8ef7940da1f63381e92db799257b
Albert Arias <47288705+wild-o@users.noreply.github.com>
2020-01-07 19:50:39 -0600

    Add files via upload

---
+++ Track.java
@@ -0,0 +1,25 @@
+package leapYear;
+import java.util.Scanner;
+
+public class Track {
+
+	public static void main(String[] args) {
+			
+			Scanner reader = new Scanner(System.in);
+
+	        System.out.println("Type a year: ");
+	        int year = Integer.parseInt(reader.nextLine());
+
+	        if (year % 4 == 0 && (!(year % 100 == 0))) {
+	            System.out.println(" The year is a leap year");
+	        } else if (year % 100 == 0 && year % 400 == 0){
+	            System.out.println("The year is a leap year.");
+	        }
+	        else{
+	            System.out.println("The year is not a leap year.");
+	        }
+	    }
+
+	}
+
+
